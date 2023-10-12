ede4d9d04adaee7ecc091d0d90bd0336435e7b32
Danny_G <danielgeremew93@gmail.com>
2023-10-12 22:24:36 +0300

    user widget finished

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,12 +1,11 @@
 import { Box, useMediaQuery } from "@mui/material";
 import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
-import UserWidget from "scenes/widgets/UserWidget";
+import UserWidget from "scenes/loginPage/widgets/UserWidget";
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
   const { _id, picturePath } = useSelector((state) => state.user);
-
 
   return (
     <Box>
